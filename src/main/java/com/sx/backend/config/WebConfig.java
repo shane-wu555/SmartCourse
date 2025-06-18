1ea2151967367baa42585842d35caf92674f426b
JiaMingFakeName <zxrjiayoua@163.com>
2025-06-18 15:08:21 +0800

    拦截相关配置（之后要加入未登录能使用的接口

---
+++ src/main/java/com/sx/backend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.sx.backend.config;
+
+import com.sx.backend.security.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/**") // 拦截所有API请求
+                .excludePathPatterns("/api/auth/**"); // 排除认证接口
+    }
+}
