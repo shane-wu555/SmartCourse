935a554baaf2bc51320fc3954adc2747aaaa96fd
shane555 <shane555@noreply.gitcode.com>
2025-06-23 17:08:48 +0800

    实现测验题库设计，支持多题型题库管理(如选择、填空、简答、编程等)，题目可关联知识图谱的知识点并可设置难度等级。

--- src/main/java/com/sx/backend/controller/QuestionBankController.java
+++ src/main/java/com/sx/backend/controller/QuestionBankController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-
+//TODO 相关接口测试并未验证
 @RestController
 @RequestMapping("/api/questionBank")
 public class QuestionBankController {
--- src/main/java/com/sx/backend/controller/QuestionController.java
+++ src/main/java/com/sx/backend/controller/QuestionController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//TODO 相关接口测试并未验证
 @RestController
 @RequestMapping("/api/question")
 public class QuestionController {
